48123ebb80bb9227ff5042cfb659c9256c114d2e
Ivan <vanrock00@yandex.ru>
2023-10-31 22:54:27 +0300

    Решена задача сортировки методом мин - макс

---
+++ Example010/Program.cs
@@ -0,0 +1,32 @@
+﻿void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} {' '}");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{   
+    for(int i = 0; i < array.Length - 1; i++)
+    {   
+        int MinIndex = i;
+        
+
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[MinIndex])
+                MinIndex = j;
+        }
+        int temp = array[i];
+        array[i] = array[MinIndex];
+        array[MinIndex] = temp;
+    }
+}
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
+
